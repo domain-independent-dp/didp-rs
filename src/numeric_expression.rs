f0c58b7dd70867b4bac46d63577c78dfd0fa9974
RK <mhgeoe@gmail.com>
2022-02-28 23:39:55 -0500

    in progress

--- src/numeric_expression.rs
+++ src/numeric_expression.rs
@@ -7,29 +7,52 @@ use crate::search_node;
 use crate::state;
 use crate::variable;
 
-pub enum NumericExpression<T: variable::Numeric> {
+pub enum NumericExpression<'a, T: variable::Numeric> {
     Number(T),
-    G,
     Variable(usize),
     ResourceVariable(usize),
+    G,
     NumericOperation(
         NumericOperator,
-        Box<NumericExpression<T>>,
-        Box<NumericExpression<T>>,
+        Box<NumericExpression<'a, T>>,
+        Box<NumericExpression<'a, T>>,
     ),
-    Cardinality(PropositionalExpression),
-    Function(NumericFunction<T>, Vec<PropositionalExpression>),
+    Cardinality(SetExpression),
+    Function1D(&'a NumericFunction1D<T>, ElementExpression),
+    Function1DSum(&'a NumericFunction1D<T>, SetExpression),
+    Function2D(
+        &'a NumericFunction2D<T>,
+        ElementExpression,
+        ElementExpression,
+    ),
+    Function2DSum(&'a NumericFunction2D<T>, SetExpression, SetExpression),
+    Function2DSumPartial1(&'a NumericFunction2D<T>, SetExpression, ElementExpression),
+    Function2DSumPartial2(&'a NumericFunction2D<T>, ElementExpression, SetExpression),
+    Function3D(
+        &'a NumericFunction3D<T>,
+        ElementExpression,
+        ElementExpression,
+        ElementExpression,
+    ),
+    Function3DSum(
+        &'a NumericFunction3D<T>,
+        SetExpression,
+        SetExpression,
+        SetExpression,
+    ),
+    Function(&'a NumericFunction<T>, Vec<ElementExpression>),
+    FunctionSum(&'a NumericFunction<T>, Vec<ArgumentExpression>),
 }
 
-impl<T: variable::Numeric> NumericExpression<T> {
+impl<'a, T: variable::Numeric> NumericExpression<'a, T> {
     pub fn eval(&self, node: &search_node::SearchNode<T>) -> T {
         match self {
             NumericExpression::Number(x) => *x,
-            NumericExpression::G => node.g,
             NumericExpression::Variable(i) => node.state.signature_variables.numeric_variables[*i],
             NumericExpression::ResourceVariable(i) => {
                 node.state.resource_variables.numeric_variables[*i]
             }
+            NumericExpression::G => node.g,
             NumericExpression::NumericOperation(op, a, b) => {
                 let a = a.eval(node);
                 let b = b.eval(node);
@@ -42,13 +65,88 @@ impl<T: variable::Numeric> NumericExpression<T> {
                     NumericOperator::Min => cmp::min(a, b),
                 }
             }
-            NumericExpression::Cardinality(expression) => match expression.eval(&node.state) {
-                Argument::Element(_) => T::one(),
-                Argument::Set(s) => T::from(s.count_ones(..)).unwrap(),
-            },
-            NumericExpression::Function(f, args) => f.eval(&args, &node.state),
+            NumericExpression::Cardinality(set) => {
+                T::from(set.eval(&node.state).count_ones(..)).unwrap()
+            }
+            NumericExpression::Function1D(f, x) => f.eval(x, &node.state),
+            NumericExpression::Function1DSum(f, x) => f.sum(x, &node.state),
+            NumericExpression::Function2D(f, x, y) => f.eval(x, y, &node.state),
+            NumericExpression::Function2DSum(f, x, y) => f.sum(x, y, &node.state),
+            NumericExpression::Function2DSumPartial1(f, x, y) => f.sum_partial1(x, y, &node.state),
+            NumericExpression::Function2DSumPartial2(f, x, y) => f.sum_partial2(x, y, &node.state),
+            NumericExpression::Function3D(f, x, y, z) => f.eval(x, y, z, &node.state),
+            NumericExpression::Function3DSum(f, x, y, z) => f.sum(x, y, z, &node.state),
+            NumericExpression::Function(f, args) => f.eval(args, &node.state),
+            NumericExpression::FunctionSum(f, args) => f.sum(args, &node.state),
         }
     }
+}
+
+pub enum ElementExpression {
+    Number(variable::ElementVariable),
+    ElementVariable(usize),
+}
+
+impl ElementExpression {
+    pub fn eval<T: variable::Numeric>(&self, state: &state::State<T>) -> variable::ElementVariable {
+        match self {
+            ElementExpression::Number(x) => *x,
+            ElementExpression::ElementVariable(i) => {
+                state.signature_variables.element_variables[*i]
+            }
+        }
+    }
+}
+
+pub enum SetExpression {
+    SetVariable(usize),
+    SetOperation(SetOperator, Box<SetExpression>, Box<SetExpression>),
+    SetElementOperation(SetElementOperator, Box<SetExpression>, ElementExpression),
+}
+
+impl SetExpression {
+    pub fn eval<T: variable::Numeric>(&self, state: &state::State<T>) -> variable::SetVariable {
+        match self {
+            SetExpression::SetVariable(i) => state.signature_variables.set_variables[*i].clone(),
+            SetExpression::SetOperation(op, a, b) => {
+                let mut a = a.eval(&state);
+                let b = b.eval(&state);
+                match op {
+                    SetOperator::Union => {
+                        a.union_with(&b);
+                        a
+                    }
+                    SetOperator::Difference => {
+                        a.difference_with(&b);
+                        a
+                    }
+                    SetOperator::Intersect => {
+                        a.intersect_with(&b);
+                        a
+                    }
+                }
+            }
+            SetExpression::SetElementOperation(op, s, e) => {
+                let mut s = s.eval(&state);
+                let e = e.eval(&state);
+                match op {
+                    SetElementOperator::Add => {
+                        s.set(e, true);
+                        s
+                    }
+                    SetElementOperator::Remove => {
+                        s.set(e, false);
+                        s
+                    }
+                }
+            }
+        }
+    }
+}
+
+pub enum ArgumentExpression {
+    Set(SetExpression),
+    Element(ElementExpression),
 }
 
 pub enum NumericOperator {
@@ -60,88 +158,133 @@ pub enum NumericOperator {
     Min,
 }
 
+pub enum SetOperator {
+    Union,
+    Difference,
+    Intersect,
+}
+
+pub enum SetElementOperator {
+    Add,
+    Remove,
+}
+
+pub struct NumericFunction1D<T: variable::Numeric>(Vec<T>);
+
+impl<T: variable::Numeric> NumericFunction1D<T> {
+    pub fn eval(&self, x: &ElementExpression, state: &state::State<T>) -> T {
+        self.0[x.eval(state)]
+    }
+
+    pub fn sum(&self, x: &SetExpression, state: &state::State<T>) -> T {
+        x.eval(state).ones().map(|x| self.0[x]).sum()
+    }
+}
+
+pub struct NumericFunction2D<T: variable::Numeric>(Vec<Vec<T>>);
+
+impl<T: variable::Numeric> NumericFunction2D<T> {
+    pub fn eval(&self, x: &ElementExpression, y: &ElementExpression, state: &state::State<T>) -> T {
+        self.0[x.eval(state)][y.eval(state)]
+    }
+
+    pub fn sum(&self, x: &SetExpression, y: &SetExpression, state: &state::State<T>) -> T {
+        let x = x.eval(state);
+        let y = y.eval(state);
+        x.ones().map(|x| y.ones().map(|y| self.0[x][y]).sum()).sum()
+    }
+
+    pub fn sum_partial1(
+        &self,
+        x: &SetExpression,
+        y: &ElementExpression,
+        state: &state::State<T>,
+    ) -> T {
+        x.eval(state)
+            .ones()
+            .zip(iter::repeat(y.eval(state)))
+            .map(|(x, y)| self.0[x][y])
+            .sum()
+    }
+
+    pub fn sum_partial2(
+        &self,
+        x: &ElementExpression,
+        y: &SetExpression,
+        state: &state::State<T>,
+    ) -> T {
+        y.eval(state)
+            .ones()
+            .zip(iter::repeat(x.eval(state)))
+            .map(|(y, x)| self.0[x][y])
+            .sum()
+    }
+}
+
+pub struct NumericFunction3D<T: variable::Numeric>(Vec<Vec<Vec<T>>>);
+
+impl<T: variable::Numeric> NumericFunction3D<T> {
+    pub fn eval(
+        &self,
+        x: &ElementExpression,
+        y: &ElementExpression,
+        z: &ElementExpression,
+        state: &state::State<T>,
+    ) -> T {
+        self.0[x.eval(state)][y.eval(state)][z.eval(state)]
+    }
+
+    pub fn sum(
+        &self,
+        x: &SetExpression,
+        y: &SetExpression,
+        z: &SetExpression,
+        state: &state::State<T>,
+    ) -> T {
+        let x = x.eval(state);
+        let y = y.eval(state);
+        let z = z.eval(state);
+        x.ones()
+            .map(|x| {
+                y.ones()
+                    .map(|y| z.ones().map(|z| self.0[x][y][z]).sum())
+                    .sum()
+            })
+            .sum()
+    }
+}
+
 pub struct NumericFunction<T: variable::Numeric>(
     collections::HashMap<Vec<variable::ElementVariable>, T>,
 );
 
 impl<T: variable::Numeric> NumericFunction<T> {
-    pub fn eval(&self, args: &[PropositionalExpression], state: &state::State<T>) -> T {
+    pub fn eval(&self, args: &[ElementExpression], state: &state::State<T>) -> T {
+        let args: Vec<variable::ElementVariable> = args.iter().map(|x| x.eval(state)).collect();
+        self.0[&args]
+    }
+
+    pub fn sum(&self, args: &[ArgumentExpression], state: &state::State<T>) -> T {
         let mut result = vec![vec![]];
         for v in args {
-            match v.eval(state) {
-                Argument::Set(s) => {
+            match v {
+                ArgumentExpression::Set(s) => {
+                    let s = s.eval(state);
                     result = result
                         .into_iter()
-                        .flat_map(|x| {
-                            iter::repeat(x).zip(s.ones()).map(|(mut x, e)| {
-                                x.push(e);
-                                x
-                            })
+                        .map(|mut v| {
+                            s.ones().for_each(|x| v.push(x));
+                            v
                         })
-                        .collect()
+                        .collect();
                 }
-                Argument::Element(e) => {
+                ArgumentExpression::Element(e) => {
                     for r in &mut result {
-                        r.push(e);
+                        r.push(e.eval(state));
                     }
                 }
             }
         }
         result.into_iter().map(|x| self.0[&x]).sum()
     }
-}
-
-pub enum PropositionalExpression {
-    ElementVariable(usize),
-    SetVariable(usize),
-    PropositonalOperation(
-        PropositionalOperator,
-        Box<PropositionalExpression>,
-        Box<PropositionalExpression>,
-    ),
-}
-
-impl PropositionalExpression {
-    pub fn eval<T: variable::Numeric>(&self, state: &state::State<T>) -> Argument {
-        match self {
-            PropositionalExpression::ElementVariable(i) => {
-                Argument::Element(state.signature_variables.element_variables[*i])
-            }
-            PropositionalExpression::SetVariable(i) => {
-                Argument::Set(state.signature_variables.set_variables[*i].clone())
-            }
-            PropositionalExpression::PropositonalOperation(op, a, b) => {
-                let a = Self::eval_argument(a, state);
-                let b = Self::eval_argument(b, state);
-                match op {
-                    PropositionalOperator::Union => Argument::Set(a.union(&b).collect()),
-                    PropositionalOperator::Difference => Argument::Set(a.difference(&b).collect()),
-                    PropositionalOperator::Intersection => {
-                        Argument::Set(a.intersection(&b).collect())
-                    }
-                }
-            }
-        }
-    }
-
-    fn eval_argument<T: variable::Numeric>(
-        argument: &PropositionalExpression,
-        state: &state::State<T>,
-    ) -> variable::SetVariable {
-        match argument.eval(state) {
-            Argument::Set(x) => x,
-            Argument::Element(x) => variable::SetVariable::new(),
-        }
-    }
-}
-
-enum Argument {
-    Set(variable::SetVariable),
-    Element(variable::ElementVariable),
-}
-
-pub enum PropositionalOperator {
-    Union,
-    Difference,
-    Intersection,
 }
