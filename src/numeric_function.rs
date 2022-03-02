8e54d8659d526315e4c234c46d3360fbe7ff1f30
RK <mhgeoe@gmail.com>
2022-03-01 20:26:51 -0500

    test for numeric function

--- src/numeric_function.rs
+++ src/numeric_function.rs
@@ -155,3 +155,198 @@ impl<T: variable::Numeric> NumericFunction<T> {
         self.0[args]
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn function_1d_eval() {
+        let f = NumericFunction1D::new(vec![10, 20, 30]);
+        assert_eq!(f.eval(0), 10);
+        assert_eq!(f.eval(1), 20);
+        assert_eq!(f.eval(2), 30);
+    }
+
+    #[test]
+    fn function_1d_sum_eval() {
+        let f = NumericFunction1D::new(vec![10, 20, 30]);
+        let mut x = variable::SetVariable::with_capacity(3);
+        x.insert(0);
+        x.insert(2);
+        assert_eq!(f.sum(&x), 40);
+    }
+
+    #[test]
+    fn function_2d_eval() {
+        let f = NumericFunction2D::new(vec![vec![10, 20, 30], vec![40, 50, 60], vec![70, 80, 90]]);
+        assert_eq!(f.eval(0, 1), 20);
+    }
+
+    #[test]
+    fn function_2d_sum_eval() {
+        let f = NumericFunction2D::new(vec![vec![10, 20, 30], vec![40, 50, 60], vec![70, 80, 90]]);
+        let mut x = variable::SetVariable::with_capacity(3);
+        x.insert(0);
+        x.insert(2);
+        let mut y = variable::SetVariable::with_capacity(3);
+        y.insert(0);
+        y.insert(1);
+        assert_eq!(f.sum(&x, &y), 180);
+    }
+
+    #[test]
+    fn function_2d_sum_x_eval() {
+        let f = NumericFunction2D::new(vec![vec![10, 20, 30], vec![40, 50, 60], vec![70, 80, 90]]);
+        let mut x = variable::SetVariable::with_capacity(3);
+        x.insert(0);
+        x.insert(2);
+        assert_eq!(f.sum_x(&x, 0), 80);
+    }
+
+    #[test]
+    fn function_2d_sum_y_eval() {
+        let f = NumericFunction2D::new(vec![vec![10, 20, 30], vec![40, 50, 60], vec![70, 80, 90]]);
+        let mut y = variable::SetVariable::with_capacity(3);
+        y.insert(0);
+        y.insert(2);
+        assert_eq!(f.sum_y(0, &y), 40);
+    }
+
+    #[test]
+    fn function_3d_eval() {
+        let f = NumericFunction3D::new(vec![
+            vec![vec![10, 20, 30], vec![40, 50, 60], vec![70, 80, 90]],
+            vec![vec![10, 20, 30], vec![40, 50, 60], vec![70, 80, 90]],
+            vec![vec![10, 20, 30], vec![40, 50, 60], vec![70, 80, 90]],
+        ]);
+        assert_eq!(f.eval(0, 1, 2), 60);
+    }
+
+    #[test]
+    fn function_3d_sum_eval() {
+        let f = NumericFunction3D::new(vec![
+            vec![vec![10, 20, 30], vec![40, 50, 60], vec![70, 80, 90]],
+            vec![vec![10, 20, 30], vec![40, 50, 60], vec![70, 80, 90]],
+            vec![vec![10, 20, 30], vec![40, 50, 60], vec![70, 80, 90]],
+        ]);
+        let mut x = variable::SetVariable::with_capacity(3);
+        x.insert(0);
+        x.insert(2);
+        let mut y = variable::SetVariable::with_capacity(3);
+        y.insert(0);
+        y.insert(1);
+        let mut z = variable::SetVariable::with_capacity(3);
+        z.insert(0);
+        z.insert(1);
+        assert_eq!(f.sum(&x, &y, &z), 240);
+    }
+
+    #[test]
+    fn function_3d_sum_x_eval() {
+        let f = NumericFunction3D::new(vec![
+            vec![vec![10, 20, 30], vec![40, 50, 60], vec![70, 80, 90]],
+            vec![vec![10, 20, 30], vec![40, 50, 60], vec![70, 80, 90]],
+            vec![vec![10, 20, 30], vec![40, 50, 60], vec![70, 80, 90]],
+        ]);
+        let mut x = variable::SetVariable::with_capacity(3);
+        x.insert(0);
+        x.insert(2);
+        assert_eq!(f.sum_x(&x, 1, 2), 120);
+    }
+
+    #[test]
+    fn function_3d_sum_y_eval() {
+        let f = NumericFunction3D::new(vec![
+            vec![vec![10, 20, 30], vec![40, 50, 60], vec![70, 80, 90]],
+            vec![vec![10, 20, 30], vec![40, 50, 60], vec![70, 80, 90]],
+            vec![vec![10, 20, 30], vec![40, 50, 60], vec![70, 80, 90]],
+        ]);
+        let mut y = variable::SetVariable::with_capacity(3);
+        y.insert(0);
+        y.insert(2);
+        assert_eq!(f.sum_y(1, &y, 2), 120);
+    }
+
+    #[test]
+    fn function_3d_sum_z_eval() {
+        let f = NumericFunction3D::new(vec![
+            vec![vec![10, 20, 30], vec![40, 50, 60], vec![70, 80, 90]],
+            vec![vec![10, 20, 30], vec![40, 50, 60], vec![70, 80, 90]],
+            vec![vec![10, 20, 30], vec![40, 50, 60], vec![70, 80, 90]],
+            vec![vec![10, 20, 30], vec![40, 50, 60], vec![70, 80, 90]],
+        ]);
+        let mut z = variable::SetVariable::with_capacity(3);
+        z.insert(0);
+        z.insert(2);
+        assert_eq!(f.sum_z(1, 2, &z), 160);
+    }
+
+    #[test]
+    fn function_3d_sum_xy_eval() {
+        let f = NumericFunction3D::new(vec![
+            vec![vec![10, 20, 30], vec![40, 50, 60], vec![70, 80, 90]],
+            vec![vec![10, 20, 30], vec![40, 50, 60], vec![70, 80, 90]],
+            vec![vec![10, 20, 30], vec![40, 50, 60], vec![70, 80, 90]],
+        ]);
+        let mut x = variable::SetVariable::with_capacity(3);
+        x.insert(0);
+        x.insert(2);
+        let mut y = variable::SetVariable::with_capacity(3);
+        y.insert(0);
+        y.insert(1);
+        assert_eq!(f.sum_xy(&x, &y, 2), 180);
+    }
+
+    #[test]
+    fn function_3d_sum_xz_eval() {
+        let f = NumericFunction3D::new(vec![
+            vec![vec![10, 20, 30], vec![40, 50, 60], vec![70, 80, 90]],
+            vec![vec![10, 20, 30], vec![40, 50, 60], vec![70, 80, 90]],
+            vec![vec![10, 20, 30], vec![40, 50, 60], vec![70, 80, 90]],
+        ]);
+        let mut x = variable::SetVariable::with_capacity(3);
+        x.insert(0);
+        x.insert(2);
+        let mut z = variable::SetVariable::with_capacity(3);
+        z.insert(0);
+        z.insert(1);
+        assert_eq!(f.sum_xz(&x, 2, &z), 300);
+    }
+
+    #[test]
+    fn function_3d_sum_yz_eval() {
+        let f = NumericFunction3D::new(vec![
+            vec![vec![10, 20, 30], vec![40, 50, 60], vec![70, 80, 90]],
+            vec![vec![10, 20, 30], vec![40, 50, 60], vec![70, 80, 90]],
+            vec![vec![10, 20, 30], vec![40, 50, 60], vec![70, 80, 90]],
+        ]);
+        let mut y = variable::SetVariable::with_capacity(3);
+        y.insert(0);
+        y.insert(2);
+        let mut z = variable::SetVariable::with_capacity(3);
+        z.insert(0);
+        z.insert(1);
+        assert_eq!(f.sum_yz(2, &y, &z), 180);
+    }
+
+    #[test]
+    fn function_eval() {
+        let mut map =
+            collections::HashMap::<Vec<variable::ElementVariable>, variable::IntegerVariable>::new(
+            );
+        let key = vec![0, 1, 0, 0];
+        map.insert(key, 100);
+        let key = vec![0, 1, 0, 1];
+        map.insert(key, 200);
+        let key = vec![0, 1, 2, 0];
+        map.insert(key, 300);
+        let key = vec![0, 1, 2, 1];
+        map.insert(key, 400);
+        let f = NumericFunction::new(map);
+        assert_eq!(f.eval(&[0, 1, 0, 0]), 100);
+        assert_eq!(f.eval(&[0, 1, 0, 1]), 200);
+        assert_eq!(f.eval(&[0, 1, 2, 0]), 300);
+        assert_eq!(f.eval(&[0, 1, 2, 1]), 400);
+    }
+}
