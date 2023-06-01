25ebf1470721a29226cc39406750e1b245cfdc0d
RK <mhgeoe@gmail.com>
2023-05-31 21:14:45 -0400

    brush up

--- dypdl-heuristic-search/src/parallel_search_algorithm/data_structure.rs
+++ dypdl-heuristic-search/src/parallel_search_algorithm/data_structure.rs
@@ -3,11 +3,11 @@
 mod arc_chain;
 mod concurrent_state_registry;
 mod search_node;
-mod successor_iterator;
+mod sendable_successor_iterator;
 
 pub use concurrent_state_registry::ConcurrentStateRegistry;
 pub use search_node::{
     CostNodeMessage, DistributedCostNode, DistributedFNode, FNodeMessage, SearchNodeMessage,
     SendableCostNode, SendableFNode,
 };
-pub use successor_iterator::SendableSuccessorIterator;
+pub use sendable_successor_iterator::SendableSuccessorIterator;
